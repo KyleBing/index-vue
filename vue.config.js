888f9cae061c1c4af7c1e6edc017b2d499fbcf54
邴新科 <kylebing@163.com>
2022-06-23 10:20:36 +0800

    add package pack

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const FileManagerPlugin = require('filemanager-webpack-plugin')
+const Moment = require('moment')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -13,4 +15,34 @@ module.exports = defineConfig({
   //     },
   //   }
   // },
+
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production'){
+      let packTimeString = new Moment().format('YYYY-MM-DD') // 打包时间
+      let plugins = []
+      plugins.push(
+          new FileManagerPlugin({
+            events: {
+              onEnd: {
+                mkdir: ['./archive'], // 新建 ./archive 目录
+                archive: [
+                  // 打包 压缩包中不带 dist 外壳
+                  {
+                    source: '../index',
+                    destination: `./archive/index-${packTimeString}.zip`,
+                    format: 'zip',
+                    options: {
+                      gzipOptions: {
+                        level: 1,
+                      },
+                    }
+                  },
+                ]
+              }
+            }
+          })
+      )
+      config.plugins = config.plugins.concat(plugins) // 将新建的 plugin 添加到原 config 中的 plugin 中
+    }
+  }
 })
