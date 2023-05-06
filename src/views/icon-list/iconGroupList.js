07fd894a829603f22c3d1e195e9f068f98790ece
邴新科-工作 <kylebing@163.com>
2023-05-06 17:45:04 +0800

    icons

--- src/views/icon-list/iconGroupList.js
+++ src/views/icon-list/iconGroupList.js
@@ -7,7 +7,6 @@ export default [
             { noPadding: true ,  isNew: false, isBgBlack: false,  pageLink: '../manager' ,                    icon: require('./icons/logo-manager.png') ,      name: '后台' ,       } ,
             { noPadding: false , isNew: false, isBgBlack: false,  pageLink: '../readme' ,                     icon: require('./icons/logo-doc.png') ,          name: 'README' ,   } ,
             { noPadding: true ,  isNew: false, isBgBlack: false,  pageLink: '../me/gif' ,                     icon: require('./icons/logo-gif.gif') ,          name: '像素画' ,     },
-            { noPadding: true ,  isNew: false, isBgBlack: false,  pageLink: '../me/gif' ,                     icon: require('./icons/logo-gif.gif') ,          name: '像素画' ,     },
             { noPadding: true,   isNew: true,  isBgBlack: false, pageLink: '../tools/highway',                icon: require('./icons/logo-web-racecar.png'),   name: '高速上的2B们',      },
         ]
     },
