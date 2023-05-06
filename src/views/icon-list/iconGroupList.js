5a3478587f3197f7538b4b15f95faa5deee0cf95
邴新科-工作 <kylebing@163.com>
2023-05-06 17:42:50 +0800

    update highway project

--- src/views/icon-list/iconGroupList.js
+++ src/views/icon-list/iconGroupList.js
@@ -3,10 +3,12 @@ export default [
         title: '我的',
         icons: [
             { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: '../diary/' ,                     icon: require('./icons/logo-diary.png') ,        name: '日记' ,     } ,
-            { noPadding: true ,  isNew: true,  isBgBlack: false,  pageLink: '../tools/map/' ,                 icon: require('./icons/logo-map.png') ,          name: '路书' ,     } ,
+            { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: '../tools/map/' ,                 icon: require('./icons/logo-map.png') ,          name: '路书' ,     } ,
             { noPadding: true ,  isNew: false, isBgBlack: false,  pageLink: '../manager' ,                    icon: require('./icons/logo-manager.png') ,      name: '后台' ,       } ,
             { noPadding: false , isNew: false, isBgBlack: false,  pageLink: '../readme' ,                     icon: require('./icons/logo-doc.png') ,          name: 'README' ,   } ,
-            { noPadding: true ,  isNew: false, isBgBlack: false,  pageLink: '../me/gif' ,                     icon: require('./icons/logo-gif.gif') ,          name: '像素画' ,     }
+            { noPadding: true ,  isNew: false, isBgBlack: false,  pageLink: '../me/gif' ,                     icon: require('./icons/logo-gif.gif') ,          name: '像素画' ,     },
+            { noPadding: true ,  isNew: false, isBgBlack: false,  pageLink: '../me/gif' ,                     icon: require('./icons/logo-gif.gif') ,          name: '像素画' ,     },
+            { noPadding: true,   isNew: true,  isBgBlack: false, pageLink: '../tools/highway',                icon: require('./icons/logo-web-racecar.png'),   name: '高速上的2B们',      },
         ]
     },
     {
@@ -32,7 +34,7 @@ export default [
     {
         title: '网易云 7日黑胶',
         icons: [
-            { noPadding: true ,  isNew: true,   isBgBlack: false,  pageLink: 'https://music.163.com/prime/m/gift-receive?d=LicTw1fi166OdFT0EswA2A&p=8Hb1nvPLegqPZqAkk1X4xlKMM_KbjkbsHSvVTKQB0fSzhZGdb4zWtvqwkPSXqaPultJrult8QWEaFdu4mzcMcmXfc4AxH7mQAEAzduTiGB0&userid=79759260&app_version=8.9.73&dlt=0846' ,                     icon: require('./icons/logo-netease-music.png') ,name: '5月' ,     } ,
+            { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://music.163.com/prime/m/gift-receive?d=LicTw1fi166OdFT0EswA2A&p=8Hb1nvPLegqPZqAkk1X4xlKMM_KbjkbsHSvVTKQB0fSzhZGdb4zWtvqwkPSXqaPultJrult8QWEaFdu4mzcMcmXfc4AxH7mQAEAzduTiGB0&userid=79759260&app_version=8.9.73&dlt=0846' ,                     icon: require('./icons/logo-netease-music.png') ,name: '5月完' ,     } ,
             { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://music.163.com/prime/m/gift-receive?d=qj0ojHhiu0XiadIY3n14Ag&p=FIrkti_v2Ux7nPAxiLTOL3_iW9PBWH1hrzpoI7lhENtS7fcgxdLyBrYzERE4B6EW0okiTwMQIODHivr4YxtXpyrYw3DswHQjl7zxSQl7ILA&userid=79759260&app_version=8.9.40&dlt=0846' ,                     icon: require('./icons/logo-netease-music.png') ,name: '4月完' ,     } ,
             { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://music.163.com/prime/m/gift-receive?d=CaP0cC3Ls3T1co9URbTgLQ&p=W4Dpr9bU3dQx_Fce4ZdCa2efPMAjgLuB8mzayY51SEa8gLyle53tgMFoe7elJQsyBiWsxzudtcr3JHHu521m6bbAgcE7aNSrr0dfN3IDRkA&userid=79759260&app_version=8.9.30&dlt=0846' ,                     icon: require('./icons/logo-netease-music.png') ,name: '3月完' ,     } ,
             { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://music.163.com/prime/m/gift-receive?d=XG_zS_50_j-DN-MTO9k9yA&p=p_E55KtdeJO5livvCYr0O0MVmIh2jJjC6hCeGii2oJbsch4o2sm8eUvEPzrLvf1QbpP_9B3Nz_vEHk0F_aHZidRNG8PC9VjwqL8u8IuPzto&userid=79759260&app_version=8.9.20&dlt=0846' ,                     icon: require('./icons/logo-netease-music.png') ,name: '2月完' ,     } ,
