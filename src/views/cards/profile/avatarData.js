2bb360b3d46910dc196127ae49e03b2a96c68a37
KyleBing(MBP14) <kylebing@163.com>
2023-05-30 23:28:55 +0800

    add avatars

--- src/views/cards/profile/avatarData.js
+++ src/views/cards/profile/avatarData.js
@@ -8,6 +8,10 @@ let avatarArray = [
     'https://diary-container.kylebing.cn/FgXyFm3gcgzASdRtiFSjTUw1arSU',
     'https://diary-container.kylebing.cn/FvZQvE2CnqGQwpiatXaQfYzrD_9h',
     'https://diary-container.kylebing.cn/Fn9VNe8MqMdftc_i8eMsVcyE3qFZ',
+    'https://diary-container.kylebing.cn/Fmb_n_Gg_yPNBM0cHwQK21T-lxJi',
+    'https://diary-container.kylebing.cn/FiBDsSuB6dG0nrtXcU6uMu-BgFBY',
+    'https://diary-container.kylebing.cn/FnkI_RzGB-QKdDAqSZn1rIRv_u-4',
+    'https://diary-container.kylebing.cn/Fti4Om4YZddvuEksWcXmBoaINXtn'
 ]
 
 export default {
