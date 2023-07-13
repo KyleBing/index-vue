c175efd1f339ab8aeee1a841566c4e0a7e335b02
邴新科-工作 <kylebing@163.com>
2023-07-13 14:48:32 +0800

    添加我的所有东西

--- src/views/icon-list/iconListData.js
+++ src/views/icon-list/iconListData.js
@@ -51,11 +51,12 @@ export default [
     {
         title: 'Canvas',
         icons: [
-            { noPadding: true ,  isNew: true,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost/?isShowLine=0&titleFontSize=50&lineWidth=10&isShowTitle=1&bgColor=black&title=+++' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离-+++' ,     } ,
-            { noPadding: true ,  isNew: true,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost/?isShowLine=0&titleFontSize=50&lineWidth=10&isShowTitle=1&bgColor=black&title=HelloWorld' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离-HW' ,     } ,
-            { noPadding: true ,  isNew: true,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost/?isShowLine=0&titleFontSize=50&lineWidth=10&isShowTitle=1&bgColor=black&title=%E6%88%91' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离-我' ,     } ,
-            { noPadding: true ,  isNew: true,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost/?isShowLine=1&titleFontSize=50&lineWidth=10&isShowTitle=0' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离-线条' ,     } ,
-            { noPadding: true ,  isNew: true,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离' ,     } ,
+            { noPadding: true ,  isNew: true,   isBgBlack: false,  pageLink: 'https://kylebing.cn/tools/mine/' ,icon: require('./icons/logo-blank.png') ,name: '我的所有' ,     } ,
+            { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost/?isShowLine=0&titleFontSize=50&lineWidth=10&isShowTitle=1&bgColor=black&title=+++' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离-+++' ,     } ,
+            { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost/?isShowLine=0&titleFontSize=50&lineWidth=10&isShowTitle=1&bgColor=black&title=HelloWorld' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离-HW' ,     } ,
+            { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost/?isShowLine=0&titleFontSize=50&lineWidth=10&isShowTitle=1&bgColor=black&title=%E6%88%91' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离-我' ,     } ,
+            { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost/?isShowLine=1&titleFontSize=50&lineWidth=10&isShowTitle=0' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离-线条' ,     } ,
+            { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-lost' ,icon: require('./icons/logo-web-canvas-lost.png') ,name: '迷离' ,     } ,
             { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-loading-canvas' ,icon: require('./icons/logo-web-canvas-loading.png') ,name: '圆圈' ,     } ,
             { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-canvas-bg' ,     icon: require('./icons/logo-web-canvas-block.png') ,  name: '活动方块' ,     } ,
             { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/test/animate-heart-canvas' ,  icon: require('./icons/logo-web-canvas-heart.png') ,  name: '飘动的心' ,     } ,
