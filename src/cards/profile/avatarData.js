717e1b2763931e5600192a031142e754066f6c19
KyleBing(MBP14) <kylebing@163.com>
2024-01-17 14:08:36 +0800

    https -> http

--- src/cards/profile/avatarData.js
+++ src/cards/profile/avatarData.js
@@ -1,25 +1,26 @@
 let avatarArray = [
-    'https://diary-container.kylebing.cn/Fkafc-ZkiXNi6UkRpqotCnpHFt_L',
-    'https://diary-container.kylebing.cn/FgpKnQMw3lz5y42OXiWkKzZ-125-',
-    'https://diary-container.kylebing.cn/FpHdzKkK-mQFCEOnapKSRElE-IDe',
-    'https://diary-container.kylebing.cn/Fn0E0Wg5VgHTF8cJamy9yZQvDrhc',
-    'https://diary-container.kylebing.cn/FugZYJCjMS_0aBia0ytnjwe71A2v',
-    'https://diary-container.kylebing.cn/Fk7dqI_LcpSX2IxFtVvfOlo5VfwC',
-    'https://diary-container.kylebing.cn/FgXyFm3gcgzASdRtiFSjTUw1arSU',
-    'https://diary-container.kylebing.cn/FvZQvE2CnqGQwpiatXaQfYzrD_9h',
-    'https://diary-container.kylebing.cn/Fn9VNe8MqMdftc_i8eMsVcyE3qFZ',
-    'https://diary-container.kylebing.cn/Fmb_n_Gg_yPNBM0cHwQK21T-lxJi',
-    'https://diary-container.kylebing.cn/FiBDsSuB6dG0nrtXcU6uMu-BgFBY',
-    'https://diary-container.kylebing.cn/Fti4Om4YZddvuEksWcXmBoaINXtn',
-    'https://diary-container.kylebing.cn/Fgww1vqigOF7EozGDxlW4LzQARKr', // 摩托车
-    'https://diary-container.kylebing.cn/FjDdoRsSdNQk6qtz1c1alOAeMF_n', // 户外草地
-    'https://diary-container.kylebing.cn/FsTSsxNbgN3QBUKPTHKu3vVEOd1X', // 湖边张开手
-    'https://diary-container.kylebing.cn/FkpVB6V4G5WsaE4RGEzSp7PLP8Nt', // 帐篷
-    'https://diary-container.kylebing.cn/FlMvjMV6nGCjyYxd44sOBikHiv3X', // 西装头盔
-    'https://diary-container.kylebing.cn/Fpxy5guJQbVuRIVgu7hCCQunMZG9', // 头盔-回家路上
-    'https://diary-container.kylebing.cn/FjqRR0LNczouHBq6HVFq-QnEaaze', // 全身-中铁汇苑
-    'https://diary-container.kylebing.cn/Fl0NVbDAV5MzukQh1ydAFpeUfldV', // 理发后
-    'https://diary-container.kylebing.cn/FqQhliCttLb7dba8oMV-pGoKNQyL', // 森林公园-自拍-拿着GoPro
+    'http://diary-container.kylebing.cn/Fkafc-ZkiXNi6UkRpqotCnpHFt_L',
+    'http://diary-container.kylebing.cn/FgpKnQMw3lz5y42OXiWkKzZ-125-',
+    'http://diary-container.kylebing.cn/FpHdzKkK-mQFCEOnapKSRElE-IDe',
+    'http://diary-container.kylebing.cn/Fn0E0Wg5VgHTF8cJamy9yZQvDrhc',
+    'http://diary-container.kylebing.cn/FugZYJCjMS_0aBia0ytnjwe71A2v',
+    'http://diary-container.kylebing.cn/Fk7dqI_LcpSX2IxFtVvfOlo5VfwC',
+    'http://diary-container.kylebing.cn/FgXyFm3gcgzASdRtiFSjTUw1arSU',
+    'http://diary-container.kylebing.cn/FvZQvE2CnqGQwpiatXaQfYzrD_9h',
+    'http://diary-container.kylebing.cn/Fn9VNe8MqMdftc_i8eMsVcyE3qFZ',
+    'http://diary-container.kylebing.cn/Fmb_n_Gg_yPNBM0cHwQK21T-lxJi',
+    'http://diary-container.kylebing.cn/FiBDsSuB6dG0nrtXcU6uMu-BgFBY',
+    'http://diary-container.kylebing.cn/Fti4Om4YZddvuEksWcXmBoaINXtn',
+    'http://diary-container.kylebing.cn/Fgww1vqigOF7EozGDxlW4LzQARKr', // 摩托车
+    'http://diary-container.kylebing.cn/FjDdoRsSdNQk6qtz1c1alOAeMF_n', // 户外草地
+    'http://diary-container.kylebing.cn/FsTSsxNbgN3QBUKPTHKu3vVEOd1X', // 湖边张开手
+    'http://diary-container.kylebing.cn/FkpVB6V4G5WsaE4RGEzSp7PLP8Nt', // 帐篷
+    'http://diary-container.kylebing.cn/FlMvjMV6nGCjyYxd44sOBikHiv3X', // 西装头盔
+    'http://diary-container.kylebing.cn/Fpxy5guJQbVuRIVgu7hCCQunMZG9', // 头盔-回家路上
+    'http://diary-container.kylebing.cn/FjqRR0LNczouHBq6HVFq-QnEaaze', // 全身-中铁汇苑
+    'http://diary-container.kylebing.cn/Fl0NVbDAV5MzukQh1ydAFpeUfldV', // 理发后
+    'http://diary-container.kylebing.cn/FqQhliCttLb7dba8oMV-pGoKNQyL', // 森林公园-自拍-拿着GoPro
+    'http://diary-container.kylebing.cn/Fiz6b-VeVmWN5iX1IZEmhA9OWtTG', // 周岁
 ]
 
 export default {
--- src/views/icon-list/mine/IconsMine.vue
+++ src/views/icon-list/mine/IconsMine.vue
@@ -13,11 +13,11 @@ export default {
                 {
                     title: '我的',
                     icons: [
-                        { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/diary/' ,                     icon: require('../icons/logo-diary.png') ,        name: '日记' ,     } ,
-                        { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/tools/map/' ,                 icon: require('../icons/logo-map.png') ,          name: '路书' ,     } ,
-                        { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/manager' ,                    icon: require('../icons/logo-manager.png') ,      name: '后台' ,       } ,
-                        { noPadding: false , isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/readme' ,                     icon: require('../icons/logo-doc.png') ,          name: 'README' ,   } ,
-                        { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'https://kylebing.cn/me/gif' ,                     icon: require('../icons/logo-gif.gif') ,          name: '像素画' ,     },
+                        { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'http://kylebing.cn/diary/' ,                     icon: require('../icons/logo-diary.png') ,        name: '日记' ,     } ,
+                        { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'http://kylebing.cn/tools/map/' ,                 icon: require('../icons/logo-map.png') ,          name: '路书' ,     } ,
+                        { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'http://kylebing.cn/manager' ,                    icon: require('../icons/logo-manager.png') ,      name: '后台' ,       } ,
+                        { noPadding: false , isNew: false,  isBgBlack: false,  pageLink: 'http://kylebing.cn/readme' ,                     icon: require('../icons/logo-doc.png') ,          name: 'README' ,   } ,
+                        { noPadding: true ,  isNew: false,  isBgBlack: false,  pageLink: 'http://kylebing.cn/me/gif' ,                     icon: require('../icons/logo-gif.gif') ,          name: '像素画' ,     },
                     ]
                 }
             ],
